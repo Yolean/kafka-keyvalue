f56f9d907f4bbc038c96dbc1495c41de1c684563
Staffan Olsson <staffan@repos.se>
2019-02-13 10:29:26 +0100

    And we have a processor

--- src/main/java/se/yolean/kafka/keyvalue/KeyvalueUpdateProcessor.java
+++ src/main/java/se/yolean/kafka/keyvalue/KeyvalueUpdateProcessor.java
@@ -2,11 +2,19 @@ package se.yolean.kafka.keyvalue;
 
 import java.util.Iterator;
 
+import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.processor.Processor;
 import org.apache.kafka.streams.processor.ProcessorContext;
+import org.apache.kafka.streams.state.KeyValueStore;
+import org.apache.kafka.streams.state.StoreBuilder;
+import org.apache.kafka.streams.state.Stores;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class KeyvalueUpdateProcessor implements KeyvalueUpdate, Processor<byte[], byte[]> {
+
+  public static final Logger logger = LoggerFactory.getLogger(KeyvalueUpdateProcessor.class);
 
 	private String sourceTopicPattern;
   private OnUpdate onUpdate;
@@ -17,11 +25,22 @@ public class KeyvalueUpdateProcessor implements KeyvalueUpdate, Processor<byte[]
 	  this.onUpdate = onUpdate;
 	}
 
+  private KeyValueStore<byte[], byte[]> getStateStore() {
+    StoreBuilder<KeyValueStore<byte[], byte[]>> storeBuilder = Stores.keyValueStoreBuilder(
+      Stores.inMemoryKeyValueStore("inmemory-counts"),
+      Serdes.ByteArray(),
+      Serdes.ByteArray()
+    );
+    return storeBuilder.build();
+  }
+
 	@Override
 	public Topology getTopology() {
 		Topology topology = new Topology();
 
 		topology.addSource("Source", sourceTopicPattern);
+
+		topology.addProcessor("KeyvalueUpdate", () -> this, "Source");
 
 		return topology;
 	}
@@ -51,8 +70,7 @@ public class KeyvalueUpdateProcessor implements KeyvalueUpdate, Processor<byte[]
 
   @Override
   public void process(byte[] key, byte[] value) {
-    // TODO Auto-generated method stub
-
+    logger.debug("Got keyvalue {}={}", new String(key), new String(value));
   }
 
   @Override
