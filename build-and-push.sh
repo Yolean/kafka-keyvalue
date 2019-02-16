3dcafaf965c5cfdba916414177249d49732cf26c
Staffan Olsson <staffan@repos.se>
2019-02-16 06:57:33 +0100

    Workaround for gradle jibDockerBuild not running java tests

--- build-and-push.sh
+++ build-and-push.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 set -e
+# TODO make jib build run the unit tests
+gradle test
 gradle jibDockerBuild --image=yolean/kafka-keyvalue:dev
 build-contract
 docker tag yolean/kafka-keyvalue:dev yolean/kafka-keyvalue:latest
