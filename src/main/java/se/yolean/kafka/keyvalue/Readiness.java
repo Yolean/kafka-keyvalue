e2a4ddaa62d51076feb4f34441c67657cf5ce6ef
Staffan Olsson <staffan@repos.se>
2019-02-16 23:38:12 +0100

    Missing in previous commit

---
+++ src/main/java/se/yolean/kafka/keyvalue/Readiness.java
@@ -0,0 +1,22 @@
+package se.yolean.kafka.keyvalue;
+
+public interface Readiness {
+
+  /**
+   * @return True if we can't find any sign of unreadiness in any component
+   */
+  boolean isAppReady();
+
+  /**
+   * Pod/container readiness is probably {@link #isAppReady()} + http started,
+   * and the latter should not happen until first app readiness.
+   */
+  void httpEnable();
+
+  /**
+   * Stops all responding to HTTP requests
+   * including, unfortunately, the /metrics endpoint.
+   */
+  void httpDisable();
+
+}
