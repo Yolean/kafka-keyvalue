6c38d705445dd35a1fdb3b3e5ffc166f4f41eea0
Staffan Olsson <staffan@repos.se>
2019-02-17 22:05:32 +0100

    Uses daemon when building locally

--- build-and-push.sh
+++ build-and-push.sh
@@ -3,9 +3,9 @@ set -e
 # TODO make jib build run the unit tests
 # If all tooling is available locally use
 #gradlejibdocker=gradle
-gradlejibdocker="docker run --rm -v $(pwd):/workspace -v /var/run/docker.sock:/var/run/docker.sock solsson/gradle-jib-docker:latest gradle"
-$gradlejibdocker --no-daemon --no-parallel --no-build-cache --stacktrace test
-$gradlejibdocker --no-daemon --no-parallel --no-build-cache --stacktrace jibDockerBuild --image=yolean/kafka-keyvalue:dev
+gradlejibdocker="docker run --rm -v $(pwd):/workspace -v /var/run/docker.sock:/var/run/docker.sock solsson/gradle-jib-docker:latest gradle --no-daemon --no-parallel --no-build-cache"
+$gradlejibdocker --stacktrace test
+$gradlejibdocker --stacktrace jibDockerBuild --image=yolean/kafka-keyvalue:dev
 build-contract
 docker tag yolean/kafka-keyvalue:dev yolean/kafka-keyvalue:latest
 docker push yolean/kafka-keyvalue:latest
