85bb66435e66d5cd6210c65810642728c43a7c76
Staffan Olsson <staffan@repos.se>
2019-02-17 22:04:47 +0100

    Scripted build now uses dockerized build tooling

--- build-and-push.sh
+++ build-and-push.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 set -e
 # TODO make jib build run the unit tests
-# TODO invent a docker image with the tooling below, to make builds reproducible
-gradle test
-gradle jibDockerBuild --image=yolean/kafka-keyvalue:dev
+# If all tooling is available locally use
+#gradlejibdocker=gradle
+gradlejibdocker="docker run --rm -v $(pwd):/workspace -v /var/run/docker.sock:/var/run/docker.sock solsson/gradle-jib-docker:latest gradle"
+$gradlejibdocker --no-daemon --no-parallel --no-build-cache --stacktrace test
+$gradlejibdocker --no-daemon --no-parallel --no-build-cache --stacktrace jibDockerBuild --image=yolean/kafka-keyvalue:dev
 build-contract
 docker tag yolean/kafka-keyvalue:dev yolean/kafka-keyvalue:latest
 docker push yolean/kafka-keyvalue:latest
