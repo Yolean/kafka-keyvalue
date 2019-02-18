eec7548beada006bc2c35bb6fae65a2564512079
Staffan Olsson <staffan@repos.se>
2019-02-18 06:18:04 +0100

    Dockerizes build-contract run too, but 'test' still results in
    Error: build-contract only wants to push to "localhost..."! Not a valid image name: yolean/kafka-keyvalue:dev

--- build-and-push.sh
+++ build-and-push.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 set -e
 # TODO make jib build run the unit tests
+
 # If all tooling is available locally use
 #gradlejibdocker=gradle
+#contract=build-contract
 gradlejibdocker="docker run --rm -v $(pwd):/workspace -v /var/run/docker.sock:/var/run/docker.sock solsson/gradle-jib-docker:latest gradle --no-daemon --no-parallel --no-build-cache"
+contract="docker run -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd)/:/source  --rm --name kafka-keyvalue-build solsson/build-contract@sha256:961624a502c4bf64bdec328e65a911a2096192e7c1a268d7360b9c85ae7a35b8"
+
 $gradlejibdocker --stacktrace test
 $gradlejibdocker --stacktrace jibDockerBuild --image=yolean/kafka-keyvalue:dev
-build-contract
+
+$contract test
+
 docker tag yolean/kafka-keyvalue:dev yolean/kafka-keyvalue:latest
 docker push yolean/kafka-keyvalue:latest
