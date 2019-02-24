6de9f3170061692e24c1f8da907be7256456e811
Staffan Olsson <staffan@repos.se>
2019-02-24 09:30:06 +0100

    Probes work a lot better without custom timeouts

--- example-nodejs-client/stack-setup.js
+++ example-nodejs-client/stack-setup.js
@@ -1,4 +1,5 @@
-// wait for services in ../build-contracts
+// Doens't actually set up anything
+// Waits for services in ../build-contracts
 
 const {
   PIXY_HOST = 'http://localhost:19090',
@@ -8,36 +9,48 @@ const {
 } = process.env;
 
 const fetch = require('node-fetch');
-
-// retry on no connection, but not on any status code
-// There's node-fetch-retry and node-fetch-plus if we want libs
-const fetchRetry = async (url, opts) => {
-  let retry = opts && opts.retries || 3
-  while (retry > 0) {
-    try {
-      return await fetch(url, opts)
-    } catch(e) {
-      if (opts.retryCallback) {
-          opts.retryCallback(retry)
-      }
-      retry = retry - 1
-      if (retry == 0) {
-          throw e
-      }
-    }
-  }
-};
+const retryx = require('retryx');
 
 module.exports = async function() {
   console.log(' Looking for test stack...');
 
-  //test("Check that pixy is online at " + PIXY_HOST, async () => {
-  let response = await fetchRetry(PIXY_HOST, {
-    timeout: 3,
-    retries: 10,
-    retryCallback: retry => console.log(new Date(), 'Retrying pixy access', retry)
+  let pixy = await retryx(() => fetch(PIXY_HOST, {
+  }),{
+    maxTries: 10,
+    beforeWait: n => console.log('Try', n, 'pixy access at', PIXY_HOST)
   });
-  //expect(response.status).toEqual(404);
-  //});
+  if (pixy.status !== 404) console.warn('Unexpected pixy response status', pixy.status, PIXY_HOST);
+  console.log('Pixy is reachable at', PIXY_HOST);
+
+  const cache = await retryx(() => fetch(`${CACHE1_HOST}/`, {
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json'
+    }
+  }),{
+    maxTries: 10,
+    beforeWait: n => console.log('Try', n, 'cache access at', CACHE1_HOST)
+  });
+  if (cache.status !== 404) console.warn('Unexpected cache service response status', cache.status, PIXY_HOST);
+  console.log('Cache service is reachable at', CACHE1_HOST);
+
+  const topic = await retryx(() => new Promise((resolve, reject) => {
+    return fetch(`${PIXY_HOST}/topics`, {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json'
+      }
+    }).then(response => {
+      return response.json().then(topics => {
+        if (!topics || !topics.length) return reject('Unexpected topics response ' + topics);
+        if (topics.indexOf(TOPIC1_NAME) === -1) return reject('Topic ' + TOPIC1_NAME + ' not found in: ' + topics);
+        console.log('Found test topic ' + TOPIC1_NAME + ' in pixy\'s topic list');
+        resolve(response);
+      });
+    });
+  }),{
+    maxTries: 10,
+    beforeWait: n => console.log('Try', n, 'topic existence', TOPIC1_NAME)
+  });
 
 };
