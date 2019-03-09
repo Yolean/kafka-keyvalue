d139fed1b2f502849aa7ae777f485cc1679d7ec8
Staffan Olsson <staffan@repos.se>
2019-03-09 13:15:16 +0100

    Adds suppression of messages that predate the processor, fixes #20

--- src/main/java/se/yolean/kafka/keyvalue/UpdateRecord.java
+++ src/main/java/se/yolean/kafka/keyvalue/UpdateRecord.java
@@ -13,11 +13,15 @@ public class UpdateRecord implements Serializable {
 
   private static final long serialVersionUID = 1L;
 
+  private static final long NO_TIMESTAMP = -1;
+
   private final TopicPartition topicPartition;
   private final long offset;
   private final String key;
   private final String string;
   private final int hashCode;
+
+  private long timestamp = NO_TIMESTAMP;
 
   @JsonCreator
   public UpdateRecord(
@@ -30,6 +34,11 @@ public class UpdateRecord implements Serializable {
     this.key = key;
     this.string = topicPartition.toString() + '-' + offset + '[' + key + ']';
     this.hashCode = string.hashCode();
+  }
+
+  public UpdateRecord(String topic, int partition, long offset, String key, long timestamp) {
+    this(topic, partition, offset, key);
+    this.timestamp  = timestamp;
   }
 
   public String getTopic() {
@@ -50,6 +59,17 @@ public class UpdateRecord implements Serializable {
 
   TopicPartition getTopicPartition() {
     return topicPartition;
+  }
+
+  /**
+   * Timestamp is just a value we carry during processing, not serialized to clients
+   * (at least not until we have a convincing use case for including it in onupdate).
+   */
+  public long getTimestamp() {
+    if (timestamp == NO_TIMESTAMP) {
+      throw new IllegalStateException("The optional timestamp was requested when not set, for " + this);
+    }
+    return timestamp;
   }
 
   @Override
