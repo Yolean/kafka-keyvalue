8825c27d15140f4983b06268de605cb08fdf6bb2
Staffan Olsson <staffan@repos.se>
2019-04-22 10:32:15 +0200

    REST @QuarkusTest fires up the entire application, which requires config

--- src/test/java/se/yolean/kafka/keyvalue/http/NativeCacheResourceIT.java
+++
@@ -1,9 +0,0 @@
-package se.yolean.kafka.keyvalue.http;
-
-import io.quarkus.test.junit.SubstrateTest;
-
-@SubstrateTest
-public class NativeCacheResourceIT extends CacheResourceTest {
-
-    // Execute the same tests but in native mode.
-}
