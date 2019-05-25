0c54e484770e2d697cf6c155caac6b5558385244
Staffan Olsson <staffan@repos.se>
2019-05-25 14:52:51 +0200

    Some clients will want different handlers for different tokens

--- src/main/java/se/yolean/kafka/keyvalue/onupdate/UpdateTarget.java
+++ src/main/java/se/yolean/kafka/keyvalue/onupdate/UpdateTarget.java
@@ -8,8 +8,11 @@ import org.apache.http.client.utils.URIUtils;
 
 public class UpdateTarget {
 
-  private HttpHost host;
-  private URI path;
+  public static final String TOPIC_NAME_REPLACE_TOKEN = "__TOPIC__";
+
+  final HttpHost host;
+  final URI path;
+  final boolean replaceToken;
 
   static URI toURI(String url) throws RuntimeException {
     try {
@@ -19,16 +22,28 @@ public class UpdateTarget {
     }
   }
 
+  static URI replaceToken(URI path, String topicName) {
+    try {
+      return new URI(path.toString().replace(TOPIC_NAME_REPLACE_TOKEN, topicName));
+    } catch (URISyntaxException e) {
+      throw new RuntimeException("Failed to insert topic name '" + topicName + "' in URI '" + path);
+    }
+  }
+
   public UpdateTarget(String url) {
     host = URIUtils.extractHost(toURI(url));
     path = toURI(url.substring(url.indexOf('/', 8)));
+    replaceToken = !path.equals(replaceToken(path, "(topic-name-would-go-here)"));
   }
 
   HttpHost getHttpclientContextHost() {
     return host;
   }
 
-  URI getHttpUriFromHost() {
+  URI getHttpUriFromHost(String topicName) {
+    if (replaceToken) {
+      return replaceToken(path, topicName);
+    }
     return path;
   }
 
--- src/test/java/se/yolean/kafka/keyvalue/onupdate/UpdateTargetTest.java
+++ src/test/java/se/yolean/kafka/keyvalue/onupdate/UpdateTargetTest.java
@@ -10,7 +10,21 @@ class UpdateTargetTest {
   void test() {
     UpdateTarget updateTarget = new UpdateTarget("http://myhost.local:12345/whatever/uri?and-params=true");
     assertEquals("http://myhost.local:12345", updateTarget.getHttpclientContextHost().toString());
-    assertEquals("/whatever/uri?and-params=true", updateTarget.getHttpUriFromHost().toString());
+    assertEquals("/whatever/uri?and-params=true", updateTarget.getHttpUriFromHost("t1").toString());
+  }
+
+  @Test
+  void testWithTopicNameInPath() {
+    UpdateTarget updateTarget = new UpdateTarget("http://dev.local:123/updates/__TOPIC__/");
+    assertEquals("http://dev.local:123", updateTarget.getHttpclientContextHost().toString());
+    assertEquals("/updates/topic1/", updateTarget.getHttpUriFromHost("topic1").toString());
+  }
+
+  @Test
+  void testWithTopicNameInQuery() {
+    UpdateTarget updateTarget = new UpdateTarget("http://dev.local:123/updates?topic=__TOPIC__");
+    assertEquals("http://dev.local:123", updateTarget.getHttpclientContextHost().toString());
+    assertEquals("/updates?topic=Topic2", updateTarget.getHttpUriFromHost("Topic2").toString());
   }
 
 }
