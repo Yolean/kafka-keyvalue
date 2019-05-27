ea68a781af55e7c59d3fea432c3586bd81c26ee5
Staffan Olsson <staffan@repos.se>
2019-05-27 06:57:25 +0200

    Sampe principle for targets as for topics, same rationale

--- src/test/java/se/yolean/kafka/keyvalue/onupdate/OnUpdateForwarderTest.java
+++ src/test/java/se/yolean/kafka/keyvalue/onupdate/OnUpdateForwarderTest.java
@@ -12,11 +12,21 @@ class OnUpdateForwarderTest {
   void testGetTargetsConfig() {
     OnUpdateForwarder forwarder = new OnUpdateForwarder();
 
-    forwarder.targetsConfig = Optional.empty();
+    forwarder.target = Optional.empty();
+    forwarder.target1 = Optional.empty();
+    forwarder.target2 = Optional.empty();
+    forwarder.target3 = Optional.empty();
+    forwarder.target4 = Optional.empty();
+    forwarder.target5 = Optional.empty();
+    forwarder.target6 = Optional.empty();
+    forwarder.target7 = Optional.empty();
+    forwarder.target8 = Optional.empty();
+    forwarder.target9 = Optional.empty();
+
     assertNotNull(forwarder.getTargetsConfig());
     assertEquals(0, forwarder.getTargetsConfig().size());
 
-    forwarder.targetsConfig = Optional.of("http://example.net/");
+    forwarder.target = Optional.of("http://example.net/");
     assertEquals(1, forwarder.getTargetsConfig().size());
     assertEquals("http://example.net/", forwarder.getTargetsConfig().get(0));
   }
