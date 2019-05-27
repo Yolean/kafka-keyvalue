2bd8e9c0f4093123ffc4e52b7e557cdb00a1223a
Staffan Olsson <staffan@repos.se>
2019-05-27 21:28:19 +0200

    Evaluate update request retries, disable ssl as workaround for native

--- src/main/java/se/yolean/kafka/keyvalue/onupdate/hc/UpdatesDispatcherHttp.java
+++ src/main/java/se/yolean/kafka/keyvalue/onupdate/hc/UpdatesDispatcherHttp.java
@@ -7,9 +7,15 @@ import org.apache.http.HttpEntity;
 import org.apache.http.HttpHost;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
 import org.apache.http.entity.ByteArrayEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.DefaultServiceUnavailableRetryStrategy;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.client.StandardHttpRequestRetryHandler;
 import org.apache.http.impl.conn.BasicHttpClientConnectionManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,6 +28,11 @@ public class UpdatesDispatcherHttp implements UpdatesDispatcher {
 
   static final Logger logger = LoggerFactory.getLogger(UpdatesDispatcherHttp.class);
 
+  int retries = 5;
+  boolean retryNonIdempotent = true;
+  int retriesOnUnavailable = 5;
+  int retriesOnUnavailableInterval = 1;
+
   ResponseHandlerAck responseHandler = new ResponseHandlerAck();
   UpdateTarget target;
   CloseableHttpClient client;
@@ -31,8 +42,21 @@ public class UpdatesDispatcherHttp implements UpdatesDispatcher {
     HttpHost host = target.getHttpclientContextHost(); // If we want to manage contexts
     logger.info("Creating http client for host {} target {}", host, target);
 
-    BasicHttpClientConnectionManager connectionManager = new BasicHttpClientConnectionManager();
-    client = HttpClients.createMinimal(connectionManager);
+    // Disable ssl for now, as we only target sidecars and native images get org.apache.http.ssl.SSLInitializationException: TLS SSLContext not available
+    Registry<ConnectionSocketFactory> registry = RegistryBuilder.<ConnectionSocketFactory>create()
+        .register("http", PlainConnectionSocketFactory.getSocketFactory())
+        //.register("https", SSLConnectionSocketFactory.getSocketFactory())
+        .build();
+    BasicHttpClientConnectionManager connectionManager = new BasicHttpClientConnectionManager(registry, null, null, null);
+
+    StandardHttpRequestRetryHandler retryHandler = new StandardHttpRequestRetryHandler(retries, retryNonIdempotent);
+    DefaultServiceUnavailableRetryStrategy serviceUnavailStrategy = new DefaultServiceUnavailableRetryStrategy(retriesOnUnavailable, retriesOnUnavailableInterval);
+
+    client = HttpClients.custom()
+        .setConnectionManager(connectionManager)
+        .setRetryHandler(retryHandler)
+        .setServiceUnavailableRetryStrategy(serviceUnavailStrategy)
+        .build();
   }
 
   @Override
