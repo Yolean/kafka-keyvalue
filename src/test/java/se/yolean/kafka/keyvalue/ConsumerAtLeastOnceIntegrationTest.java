2c7a0d920bb25a9191f6fdbf47917c8b37637040
Staffan Olsson <staffan@repos.se>
2019-05-27 15:05:37 +0200

    Removes an unused import

--- src/test/java/se/yolean/kafka/keyvalue/ConsumerAtLeastOnceIntegrationTest.java
+++ src/test/java/se/yolean/kafka/keyvalue/ConsumerAtLeastOnceIntegrationTest.java
@@ -28,7 +28,6 @@ import org.mockito.Mockito;
 import com.salesforce.kafka.test.junit5.SharedKafkaTestResource;
 
 import se.yolean.kafka.keyvalue.ConsumerAtLeastOnce.Stage;
-import se.yolean.kafka.keyvalue.metrics.KafkaMetrics;
 
 public class ConsumerAtLeastOnceIntegrationTest {
 
