27b6e9dcbc943e7f34c43e26981bbd08414c7c25
Anton Lindgren <anton.lindgren@yolean.com>
2020-02-25 16:41:08 +0100

    add KafkaKeyvalue.putOther(...) as a way to write to another kafka topic than the provided one

--- src/sidecar.itest.ts
+++ src/sidecar.itest.ts
@@ -20,6 +20,47 @@ describe('waiting for initial cache readiness', function () {
     server.close(done);
   });
 
+  beforeEach(() => {
+    promClient.register.clear();
+  });
+
+  it('is a lightweight way to let us write to any topic we can specify', async function () {
+
+    const metrics = KafkaKeyValue.createMetrics(promClient.Counter, promClient.Gauge, promClient.Histogram);
+    
+    const cache1 = new KafkaKeyValue({
+      cacheHost: 'http://localhost:8091',
+      metrics,
+      pixyHost: 'http://pixy',
+      topicName: 'sidecar-itest'
+    });
+
+    await cache1.onReady();
+
+    const cache2 = new KafkaKeyValue({
+      cacheHost: 'http://localhost:8092',
+      metrics,
+      pixyHost: 'http://pixy',
+      topicName: 'sidecar-itest-other'
+    });
+
+    await cache2.onReady();
+
+    const key = getUniqueKey('write_to_other');
+
+    const updateReceived = new Promise(resolve => cache2.onUpdate((updatedKey, value) => {
+      if (updatedKey === key) resolve();
+    }));
+
+    const updateWritten = cache1.putOther('sidecar-itest-other', key, { x: 'y' });
+
+    await updateWritten;
+    await updateReceived;
+
+    const value = await cache2.get(key);
+    expect(value).toEqual({ x: 'y' });
+  });
+
   it('lets us put and get values afterwards', async function () {
     const cache1 = new KafkaKeyValue({
       cacheHost: 'http://localhost:8091',
