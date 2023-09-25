2645dcfadd3e5fe7471d70f2caa6fa4779ff8f9e
Staffan Olsson <staffan@repos.se>
2023-09-25 10:34:42 +0200

    Deduplication idea, experimental build

--- src/test/java/se/yolean/kafka/keyvalue/ConsumerAtLeastOnceTest.java
+++ src/test/java/se/yolean/kafka/keyvalue/ConsumerAtLeastOnceTest.java
@@ -15,19 +15,19 @@ public class ConsumerAtLeastOnceTest {
     var registry = new SimpleMeterRegistry();
     var instance = new ConsumerAtLeastOnce(registry);
     assertFalse(registry.getMetersAsString().contains("17"));
-    instance.toStats(new UpdateRecord("mytopic", 0, 17, "key1", 100));
+    instance.toStats(new UpdateRecord("mytopic", 0, 17, "key1", 100), false);
     assertTrue(registry.getMetersAsString().contains("17"));
-    instance.toStats(new UpdateRecord("mytopic", 0, 27, "key1", 100));
+    instance.toStats(new UpdateRecord("mytopic", 0, 27, "key1", 100), false);
     assertFalse(registry.getMetersAsString().contains("17"));
     assertFalse(registry.getMetersAsString().contains("NaN"));
     assertTrue(registry.getMetersAsString().contains("27"));
-    instance.toStats(new UpdateRecord("mytopic", 0, 30, "key1", 100));
+    instance.toStats(new UpdateRecord("mytopic", 0, 30, "key1", 100), false);
     assertTrue(registry.getMetersAsString().contains("30"));
-    instance.toStats(new UpdateRecord("mytopic", 0, 31, "key1", 100));
+    instance.toStats(new UpdateRecord("mytopic", 0, 31, "key1", 100), false);
     assertTrue(registry.getMetersAsString().contains("31"));
-    instance.toStats(new UpdateRecord("mytopic", 0, 32, "key1", 100));
+    instance.toStats(new UpdateRecord("mytopic", 0, 32, "key1", 100), false);
     assertTrue(registry.getMetersAsString().contains("32"));
-    instance.toStats(new UpdateRecord("mytopic", 0, 33, "key1", 100));
+    instance.toStats(new UpdateRecord("mytopic", 0, 33, "key1", 100), false);
     assertTrue(registry.getMetersAsString().contains("33"));
   }
 
