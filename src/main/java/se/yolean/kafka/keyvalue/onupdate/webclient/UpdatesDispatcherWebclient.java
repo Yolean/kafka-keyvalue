606a8b2eacbf7347aecd8be974fd32918267568f
NiklasJonsson6 <bknjniklas@gmail.com>
2023-10-20 14:54:35 +0200

    restore the optional static target
    only one for now, in addition to endpoints

--- src/main/java/se/yolean/kafka/keyvalue/onupdate/webclient/UpdatesDispatcherWebclient.java
+++ src/main/java/se/yolean/kafka/keyvalue/onupdate/webclient/UpdatesDispatcherWebclient.java
@@ -2,6 +2,7 @@ package se.yolean.kafka.keyvalue.onupdate.webclient;
 
 import java.time.Duration;
 import java.util.Map;
+import java.util.function.Consumer;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
@@ -10,6 +11,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import io.micrometer.core.instrument.MeterRegistry;
+import io.smallrye.mutiny.Uni;
 import io.vertx.core.buffer.Buffer;
 import io.vertx.core.json.JsonObject;
 import io.vertx.mutiny.core.MultiMap;
@@ -50,34 +52,48 @@ public class UpdatesDispatcherWebclient implements UpdatesDispatcher {
     });
   }
 
+  @Override
   public void dispatch(UpdatesBodyPerTopic body) {
     dispatch(body, watcher.getTargets());
   }
 
   private void dispatch(UpdatesBodyPerTopic body, Map<String, String> targets) {
-
     watcher.updateUnreadyTargets(body);
 
     Map<String, String> headers = body.getHeaders();
     JsonObject json = new JsonObject(Buffer.buffer(body.getContent()));
-    targets.entrySet().parallelStream().forEach(entry -> {
+
+    if (config.targetStaticHost().isPresent()) {
+      String host = config.targetStaticHost().orElseThrow();
+      int port = config.targetStaticPort();
+      dispatch(json, headers, host, port).subscribe().with(item -> {
+        logger.info("Successfully sent update to static host: {}", host);
+      }, getDispatchFailureConsumer("static host: " + host));
+    }
+
+    targets.entrySet().stream().forEach(entry -> {
       String ip = entry.getKey();
       String name = entry.getValue();
-      webClient
-          .post(config.targetServicePort(), ip, config.targetPath())
-          .putHeaders(MultiMap.caseInsensitiveMultiMap().addAll(headers))
-          .sendJsonObject(json)
-          .onFailure().retry().withBackOff(Duration.ofSeconds(config.retryBackoffSeconds())).atMost(config.retryTimes())
-          .subscribe().with(
-            item -> {
-              logger.info("Successfully sent update to {}", name);
-            },
-            failure -> {
-              registry.counter("kkv.target.update.failure").increment();
-              logger.error("Failed to send update to " + name, failure);
-            }
-          );
+
+      dispatch(json, headers, ip, config.targetServicePort()).subscribe().with(item -> {
+        logger.info("Successfully sent update to {}", name);
+      }, getDispatchFailureConsumer(name));
     });
+  }
+
+  private Uni<?> dispatch(JsonObject json, Map<String, String> headers, String host, int port) {
+    return webClient
+      .post(config.targetServicePort(), host, config.targetPath())
+      .putHeaders(MultiMap.caseInsensitiveMultiMap().addAll(headers))
+      .sendJsonObject(json)
+      .onFailure().retry().withBackOff(Duration.ofSeconds(config.retryBackoffSeconds())).atMost(config.retryTimes());
+  }
+
+  private Consumer<Throwable> getDispatchFailureConsumer(String name) {
+    return (t) -> {
+      registry.counter("kkv.target.update.failure").increment();
+      logger.error("Failed to send update to " + name, t);
+    };
   }
 
   @Override
--- src/main/java/se/yolean/kafka/keyvalue/onupdate/webclient/UpdatesDispatcherWebclientConfig.java
+++ src/main/java/se/yolean/kafka/keyvalue/onupdate/webclient/UpdatesDispatcherWebclientConfig.java
@@ -1,5 +1,7 @@
 package se.yolean.kafka.keyvalue.onupdate.webclient;
 
+import java.util.Optional;
+
 import io.smallrye.config.ConfigMapping;
 import io.smallrye.config.WithName;
 
@@ -11,6 +13,12 @@ public interface UpdatesDispatcherWebclientConfig {
 
   @WithName("target.path")
   String targetPath();
+
+  @WithName("target.static.host")
+  Optional<String> targetStaticHost();
+
+  @WithName("target.static.port")
+  int targetStaticPort();
 
   @WithName("dispatcher.retry.times")
   long retryTimes();
