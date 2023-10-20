f236015f6d1e1ab19da33e6a03253e410f803adb
NiklasJonsson6 <bknjniklas@gmail.com>
2023-10-20 14:51:57 +0200

    unused inject removed

--- src/main/java/se/yolean/kafka/keyvalue/onupdate/OnUpdateForwarder.java
+++ src/main/java/se/yolean/kafka/keyvalue/onupdate/OnUpdateForwarder.java
@@ -27,15 +27,11 @@ import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
 import se.yolean.kafka.keyvalue.OnUpdate;
 import se.yolean.kafka.keyvalue.UpdateRecord;
-import se.yolean.kafka.keyvalue.kubernetes.EndpointsWatcher;
 
 @ApplicationScoped
 public class OnUpdateForwarder implements OnUpdate {
 
   static final Logger logger = LoggerFactory.getLogger(OnUpdateForwarder.class);
-
-  @Inject
-  EndpointsWatcher watcher;
 
   @Inject
   DispatcherConfig dispatcherConfig;
